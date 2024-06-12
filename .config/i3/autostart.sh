24b1a9c61a47864305cd5bb6b9c04f61d7d0d4c0
SniperSlayerss <sniperslayer122@gmail.com>
2024-06-13 00:38:28 +0100

    Added i3

---
+++ .config/i3/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Load arandr screen layout
+~/.screenlayout/defaultLayout.sh
+
+# Set NVIDIA settings
+nvidia-settings --assign CurrentMetaMode="3440x1440_175+0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+
