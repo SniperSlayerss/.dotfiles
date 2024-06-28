c50958cea216cdac06b2af486ae4a001f989ac5f
Jack <jack_baker42@hotmail.co.uk>
2024-06-28 21:08:38 +0100

    Update hypr configs

--- common/.config/nvim/lua/sniperslayerss/lazy/init.lua
+++ common/.config/nvim/lua/sniperslayerss/lazy/init.lua
@@ -2,13 +2,9 @@ return {
 
     {
         "nvim-lua/plenary.nvim",
-        name = "plenary"
+        name = "plenary",
     },
 
     "eandrju/cellular-automaton.nvim",
-    "alanfortlink/blackjack.nvim",
-    "rktjmp/playtime.nvim",
-    "seandewar/nvimesweeper",
-    "alec-gibson/nvim-tetris",
 }
 
