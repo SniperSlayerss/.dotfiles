31fc11861da15a68caff8c727dd43e01dd4ddef4
SniperSlayerss <sniperslayer122@gmail.com>
2024-07-04 07:17:22 +0100

    Added pc specific files

--- common/.config/nvim/lua/sniperslayerss/lazy/vim-with-me.lua
+++
@@ -1,6 +0,0 @@
-return {
-    {
-        "ThePrimeagen/vim-with-me",
-        config = function() end
-    },
-}
--- common/.config/nvim/lua/sniperslayerss/remap.lua
+++ common/.config/nvim/lua/sniperslayerss/remap.lua
@@ -32,7 +32,6 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set(
     "n",
     "<leader>ee",
@@ -42,48 +41,38 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
+
+-- Set keymap options
+local opts = { noremap = true, silent = true }
 -- BarBar
 local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<D-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<D-.>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<D-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<D->>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '¹', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '²', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '³', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '€', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '½', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '¾', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<D-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<D-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<D-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<D-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<D-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<D-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<D-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<D-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<D-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<D-0>', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+map('n', '<D-p>', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
--- Wipeout buffer
---                 :BufferWipeout
--- Close commands
---                 :BufferCloseAllButCurrent
---                 :BufferCloseAllButPinned
---                 :BufferCloseAllButCurrentOrPinned
---                 :BufferCloseBuffersLeft
---                 :BufferCloseBuffersRight
+map('n', '<D-c>', '<Cmd>BufferClose<CR>', opts)
 -- Magic buffer-picking mode
-map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+map('n', '<D-p>', '<Cmd>BufferPick<CR>', opts)
 -- Sort automatically by...
 map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
 map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
 map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
 map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
 map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
-
--- Other:
--- :BarbarEnable - enables barbar (enabled by default)
--- :BarbarDisable - very bad command, should never be used
