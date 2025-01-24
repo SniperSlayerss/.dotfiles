82f9e0f1ec7f0855df72d4dc6db35fba5cd6e2d9
Jack Baker <jack.baker@cambridgeconsultants.com>
2025-01-24 12:19:22 +0000

    .emacs

---
+++ shared/.emacs.custom/require.el
@@ -0,0 +1,21 @@
+;; Add MELPA repository
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Initialize package system
+(package-initialize)
+
+;; Ensure the package list is updated
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(defun require-one-package (package)
+    (unless (package-installed-p 'package)
+    (package-refresh-contents)
+    (package-install 'package))
+    (require 'package))
+
+(defun require-package (&rest packages)
+  (dolist (package packages)
+    (require-one-package package)))
