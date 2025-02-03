996bcddf07c0f8838f3382b6eb2e27320e937761
Jack Baker <jack.baker@cambridgeconsultants.com>
2025-02-03 14:41:15 +0000

    nvim project.nvim, telescope

---
+++ shared/.config/nvim/lua/sniperslayerss/lazy/compile.lua
@@ -0,0 +1,10 @@
+return {
+  "ej-shafran/compile-mode.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    vim.g.compile_mode = {
+    }
+  end
+}
--- shared/.config/nvim/lua/sniperslayerss/lazy/telescope.lua
+++ shared/.config/nvim/lua/sniperslayerss/lazy/telescope.lua
@@ -1,47 +1,99 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+  tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    config = function()
-        local actions = require("telescope.actions")
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
+    "ahmedkhalf/project.nvim",
+  },
 
-        local telescope = require('telescope')
+  config = function()
+    local actions = require("telescope.actions")
+    local telescope = require("telescope")
 
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    n = {
-                        ["<C-c>"] = actions.close,
-                    },
-                },
-                previewer = false,
-            },
-            pickers = {
-                find_files = {
-                    theme = "ivy",
-                    previewer = false,
-                },
-            },
-        })
+    -- Load project.nvim before setting up telescope
+    require("project_nvim").setup({
+      detection_methods = { "pattern", "lsp" },
+      patterns = { ".git", "Makefile", "package.json", "pyproject.toml", ".projectile" },
+      manual_mode = false,
+      silent_chdir = false,
+      scope_chdir = "global",
+    })
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    telescope.setup({
+      defaults = {
+        mappings = {
+          n = {
+            ["<C-c>"] = actions.close,
+          },
+        },
+
+        layout_strategy = 'bottom_pane',
+        layout_config = {
+          height = 0.25,
+          preview_cutoff = 20,
+          prompt_position = "bottom"
+        },
+        previewer = false,
+      },
+      pickers = {
+        find_files = {
+          previewer = false,
+        },
+        git_files = {
+          previewer = false,
+        },
+      },
+      extensions = {
+        projects = {},
+      },
+    })
+
+    -- Load the project extension
+    telescope.load_extension("projects")
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+    vim.keymap.set("n", "<leader>pws", function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set("n", "<leader>pWs", function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set("n", "<leader>ps", function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+    -- Add a keybinding for listing projects
+    vim.keymap.set("n", "<leader>pp", function()
+      require("telescope").extensions.projects.projects({
+        attach_mappings = function(_, map)
+          map("i", "<CR>", function(prompt_bufnr)
+            local action_state = require("telescope.actions.state")
+            local actions = require("telescope.actions")
+            local selected_entry = action_state.get_selected_entry()
+            local project_path = selected_entry.value
+
+            actions.close(prompt_bufnr)
+            vim.cmd("cd " .. project_path)
+
+            -- Open find_files including hidden & ignored files
+            require("telescope.builtin").find_files({
+              cwd = project_path,
+              hidden = true,
+              no_ignore = true,
+              file_ignore_patterns = { ".git/" },
+            })
+          end)
+          return true
+        end,
+      })
+    end, { desc = "Find and Search Projects" })
+  end
 }
