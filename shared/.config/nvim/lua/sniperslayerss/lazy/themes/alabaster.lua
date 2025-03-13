57c5e4279e084f24c8fd6c78b647741d2a62de33
Jack Baker <sniperslayer122@gmail.com>
2025-03-13 22:39:20 +0000

    nvim: better theme setup

---
+++ shared/.config/nvim/lua/sniperslayerss/lazy/themes/alabaster.lua
@@ -0,0 +1,7 @@
+return {
+  "p00f/alabaster.nvim",
+  config = function()
+    vim.o.background = "light"
+    vim.cmd.colorscheme("alabaster")
+  end
+}
---
+++ shared/.config/nvim/lua/sniperslayerss/lazy/themes/rose-pine-dawn.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require('rose-pine').setup({
+        variant = "dawn",
+        disable_background = false,
+        dim_inactive_windows = false,
+        styles = {
+          italic = false,
+        },
+        groups = {
+          background = '#ffffff'
+        },
+      })
+
+      vim.cmd.colorscheme("rose-pine-dawn")
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
+  },
+}
